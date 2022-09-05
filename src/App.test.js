0998cc81f689c2f2d7647e3916c0e75be5675977
Kymarndo <49214298+Kymarndo@users.noreply.github.com>
2022-09-05 22:06:56 +1200

    Router set up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+function placeholder(){
+return('hello');
+}
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('placeholder test', () => {
+  expect(placeholder().toBe('hello'))
 });
