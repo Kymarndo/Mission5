d2d58b1d232141633e645cea273f9e322ff06527
Kymarndo <49214298+Kymarndo@users.noreply.github.com>
2022-09-08 20:53:02 +1200

    Quote Form Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import RegoSearch from "../components/home/rego-search/RegoSearch";
 import Table from "../components/home/table/Table";
 import OptionalInsurance from "../components/home/optional-insurance/OptionalInsurance";
 
+
+
 export default function Home() {
   return (
     <div>
-      <RegoSearch />
+       <RegoSearch />
       <Table />
-      <OptionalInsurance/>
+      <OptionalInsurance/> 
     </div>
   );
 }
